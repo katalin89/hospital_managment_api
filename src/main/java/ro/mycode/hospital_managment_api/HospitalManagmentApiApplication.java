404eda471a9413916bf9e611da258a95acf1819a
kati1989 <kati_1989@yahoo.com>
2023-04-09 11:57:41 +0300

    first commit

---
+++ src/main/java/ro/mycode/hospital_managment_api/HospitalManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.hospital_managment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/hospital_managment_api/HospitalManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.hospital_managment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
